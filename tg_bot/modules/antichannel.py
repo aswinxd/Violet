1b8006e2ba9cad270bde9f216085cae7309bcb61
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-01-21 20:27:16 +0530

    Update antichannel.py

--- tg_bot/modules/antichannel.py
+++ tg_bot/modules/antichannel.py
@@ -40,3 +40,5 @@ def eliminate_channel(update: Update, context: CallbackContext):
         message.delete()
         sender_chat = message.sender_chat
         bot.ban_chat_sender_chat(sender_chat_id=sender_chat.id, chat_id=chat.id)
+
+__mod_name__ = "Anti-channel"
