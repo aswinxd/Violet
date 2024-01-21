0dde6e134df8a7a39934ef0700556309c5929eed
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-01-21 19:38:17 +0530

    Update disable.py

--- tg_bot/modules/disable.py
+++ tg_bot/modules/disable.py
@@ -249,22 +249,6 @@ if is_module_loaded(FILENAME):
     def __chat_settings__(chat_id, user_id):
         return build_curr_disabled(chat_id)
 
-    __mod_name__ = "Disabling"
-
-    __help__ = """
-Not everyone wants every feature that the bot offers. Some commands are best \
-left unused; to avoid spam and abuse.
-
-This allows you to disable some commonly used commands, so noone can use them. \
-It'll also allow you to autodelete them, stopping people from bluetexting.
-
- • /cmds: Check the current status of disabled commands
-
-*Admin only:*
- • /enable <cmd name>: Enable that command
- • /disable <cmd name>: Disable that command
- • /listcmds: List all possible disablable commands
-    """
 
     DISABLE_HANDLER = CommandHandler(
         "disable", disable, pass_args=True, run_async=True
@@ -288,7 +272,3 @@ else:
     DisableAbleCommandHandler = CommandHandler
     DisableAbleMessageHandler = MessageHandler
 
-from tg_bot.modules.language import gs
-
-def get_help(chat):
-    return gs(chat, "disable_help")
