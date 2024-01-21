e1b0078ab52cbf9eb3e3cf814203d36b45e10ea0
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-01-21 15:05:18 +0530

    Update misc.py

--- tg_bot/modules/helper_funcs/misc.py
+++ tg_bot/modules/helper_funcs/misc.py
@@ -86,26 +86,33 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
             )
         )
 
-    COLUMN_SIZE = 14
+    COLUMN_SIZE = 4
 
     max_num_pages = ceil(len(pairs) / COLUMN_SIZE)
     modulo_page = page_n % max_num_pages
 
- if len(pairs) > COLUMN_SIZE:
+    # can only have a certain amount of buttons side by side
+    if len(pairs) > COLUMN_SIZE:
         pairs = pairs[
             modulo_page * COLUMN_SIZE : COLUMN_SIZE * (modulo_page + 1)
         ] + [
             (
                 EqInlineKeyboardButton(
+                    "❮",
+                    callback_data="{}_prev({})".format(prefix, modulo_page),
+                ),
+                EqInlineKeyboardButton(
                     "Back",
                     callback_data="start_back",
+                ),
+                EqInlineKeyboardButton(
+                    "❯",
+                    callback_data="{}_next({})".format(prefix, modulo_page),
                 ),
             )
         ]
 
     return pairs
-
-
 
 def article(
     title: str = "",
