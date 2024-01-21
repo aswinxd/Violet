a0cc417ff90689e9c022b4eb8df24b45079b62c0
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-01-21 11:48:11 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -79,9 +79,9 @@ class KigyoINIT:
         self.WALL_API: str = self.parser.get('WALL_API', None)
         self.LASTFM_API_KEY: str = self.parser.get('LASTFM_API_KEY', None)
         self.CF_API_KEY: str =  self.parser.get("CF_API_KEY", None)
-        self.bot_id = 0 #placeholder
-        self.bot_name = "Kigyo" #placeholder
-        self.bot_username = "KigyoRobot" #placeholder
+        self.BOT_ID = 0 #placeholder
+        self.BOT_NAME = "Kigyo" #placeholder
+        self.BOT_USERNAME = "KigyoRobot" #placeholder
         self.DEBUG: bool = self.parser.getboolean("IS_DEBUG", False)
         self.DROP_UPDATES: bool = self.parser.getboolean("DROP_UPDATES", True)
         self.BOT_API_URL: str = self.parser.get('BOT_API_URL', "https://api.telegram.org/bot")
