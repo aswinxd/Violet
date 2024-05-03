d3953e5b4226c23ea289bbdec90e6bdf82c74362
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-05-03 15:56:17 +0530

    Update welcome.py

--- tg_bot/modules/welcome.py
+++ tg_bot/modules/welcome.py
@@ -29,8 +29,7 @@ from tg_bot.modules.helper_funcs.string_handling import (
     markdown_parser,
 )
 from tg_bot.modules.log_channel import loggable
-from tg_bot.modules.sql.clear_cmd_sql import get_clearcmd
-from tg_bot.modules.sql.global_bans_sql import is_user_gbanned
+from tg_bot.modules.sql.cleaner_sql import get_clearcmd
 from telegram import (
     ChatPermissions,
     InlineKeyboardButton,
