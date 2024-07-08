6b664c4a67e85149b98c98274a919faad22229a7
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-07-09 00:01:38 +0530

    Update disable_sql.py

--- tg_bot/modules/sql/disable_sql.py
+++ tg_bot/modules/sql/disable_sql.py
@@ -1,9 +1,13 @@
 import threading
-
 from sqlalchemy import Column, String, UnicodeText, func, distinct
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session, Query
+from sqlalchemy import create_engine
+from tg_bot import DB_URI, KInit, log
 
-from tg_bot.modules.sql import SESSION, BASE
-
+BASE = declarative_base()
+DISABLE_INSERTION_LOCK = threading.RLock()
+DISABLED = {}
 
 class Disable(BASE):
     __tablename__ = "disabled_commands"
@@ -17,12 +21,26 @@ class Disable(BASE):
     def __repr__(self):
         return "Disabled cmd {} in {}".format(self.command, self.chat_id)
 
+def start() -> scoped_session:
+    try:
+        engine = create_engine(DB_URI, echo=KInit.DEBUG)
+        log.info("[MySQL] Connecting to database......")
+        BASE.metadata.bind = engine
+        BASE.metadata.create_all(engine, checkfirst=True)
+        log.info("[MySQL] Database schema created or verified.")
+        return scoped_session(
+            sessionmaker(bind=engine, autoflush=False, query_cls=CachingQuery)
+        )
+    except Exception as e:
+        log.exception(f"[MySQL] Failed to connect due to {e}")
+        exit(1)
 
-Disable.__table__.create(checkfirst=True)
-DISABLE_INSERTION_LOCK = threading.RLock()
-
-DISABLED = {}
-
+try:
+    SESSION: scoped_session = start()
+    log.info("[MySQL] Connection successful, session started.")
+except Exception as e:
+    log.exception(f"[MySQL] An error occurred: {e}")
+    exit(1)
 
 def disable_command(chat_id, disable):
     with DISABLE_INSERTION_LOCK:
@@ -39,7 +57,6 @@ def disable_command(chat_id, disable):
         SESSION.close()
         return False
 
-
 def enable_command(chat_id, enable):
     with DISABLE_INSERTION_LOCK:
         disabled = SESSION.query(Disable).get((str(chat_id), enable))
@@ -55,14 +72,11 @@ def enable_command(chat_id, enable):
         SESSION.close()
         return False
 
-
 def is_command_disabled(chat_id, cmd):
     return str(cmd).lower() in DISABLED.get(str(chat_id), set())
 
-
 def get_all_disabled(chat_id):
     return DISABLED.get(str(chat_id), set())
-
 
 def num_chats():
     try:
@@ -70,13 +84,11 @@ def num_chats():
     finally:
         SESSION.close()
 
-
 def num_disabled():
     try:
         return SESSION.query(Disable).count()
     finally:
         SESSION.close()
-
 
 def migrate_chat(old_chat_id, new_chat_id):
     with DISABLE_INSERTION_LOCK:
@@ -90,16 +102,13 @@ def migrate_chat(old_chat_id, new_chat_id):
 
         SESSION.commit()
 
-
 def __load_disabled_commands():
     global DISABLED
     try:
         all_chats = SESSION.query(Disable).all()
         for chat in all_chats:
             DISABLED.setdefault(chat.chat_id, set()).add(chat.command)
-
     finally:
         SESSION.close()
-
 
 __load_disabled_commands()
